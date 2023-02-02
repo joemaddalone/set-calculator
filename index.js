cfec27a095e717e87e892eef2a56a92b901c3e0b
joemaddalone <joe.maddalone@gmail.com>
2023-02-01 22:36:05 -0600

    latest react

--- index.js
+++ index.js
@@ -1,86 +1,58 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { Input, Container, Table, Button } from "semantic-ui-react";
 import { sets } from "./calc";
-class SetCalculator extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      weight: 0,
-      week: "5",
-    };
-    this.weightInput = null;
-    this.weekInput = null;
-  }
-  onChange(e) {
-    this.setState({
-      weight: this.weightInput.inputRef.value,
-    });
-  }
-  render() {
-    return (
-      <Container>
-        <Input
-          onChange={this.onChange.bind(this)}
-          ref={(el) => (this.weightInput = el)}
-          placeholder="training max"
-        />
-        <Button.Group>
-          <Button
-            active={this.state.week === "5"}
-            onClick={() => this.setState({ week: "5" })}
-          >
-            Week 1
-          </Button>
-          <Button
-            active={this.state.week === "3"}
-            onClick={() => this.setState({ week: "3" })}
-          >
-            Week 2
-          </Button>
-          <Button
-            active={this.state.week === "1"}
-            onClick={() => this.setState({ week: "1" })}
-          >
-            Week 3
-          </Button>
-          <Button
-            active={this.state.week === "D"}
-            onClick={() => this.setState({ week: "D" })}
-          >
-            Deload
-          </Button>
-        </Button.Group>
-        {this.state.weight ? (
-          <div>
-            <Table celled>
-              <Table.Header>
-                <Table.Row>
-                  <Table.HeaderCell>Weight</Table.HeaderCell>
-                  <Table.HeaderCell>Reps</Table.HeaderCell>
-                  <Table.HeaderCell>Plates</Table.HeaderCell>
+
+const SetCalculator = () => {
+  const [weight, setWeight] = React.useState(0);
+  const [week, setWeek] = React.useState("5");
+  const weightInput = React.useRef(null);
+  const onChange = (e) => {
+    setWeight(weightInput.current.inputRef.value);
+  };
+  return (
+    <Container>
+      <Input onChange={onChange} ref={weightInput} placeholder="training max" />
+      <Button.Group>
+        <Button active={week === "5"} onClick={() => setWeek("5")}>
+          Week 1
+        </Button>
+        <Button active={week === "3"} onClick={() => setWeek("3")}>
+          Week 2
+        </Button>
+        <Button active={week === "1"} onClick={() => setWeek("1")}>
+          Week 3
+        </Button>
+        <Button active={week === "D"} onClick={() => setWeek("D")}>
+          Deload
+        </Button>
+      </Button.Group>
+      {weight ? (
+        <div>
+          <Table celled>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>Weight</Table.HeaderCell>
+                <Table.HeaderCell>Reps</Table.HeaderCell>
+                <Table.HeaderCell>Plates</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              {sets(weight, week).map((set, i) => (
+                <Table.Row key={i}>
+                  <Table.Cell>{set.weight}</Table.Cell>
+                  <Table.Cell>{set.reps}</Table.Cell>
+                  <Table.Cell>{set.plates.join(", ")}</Table.Cell>
                 </Table.Row>
-              </Table.Header>
+              ))}
+            </Table.Body>
+          </Table>
+        </div>
+      ) : null}
+    </Container>
+  );
+};
 
-              <Table.Body>
-                {sets(this.state.weight, this.state.week).map(
-                  ({ weight, reps, plates }, i) => {
-                    return (
-                      <Table.Row key={i}>
-                        <Table.Cell>{weight}</Table.Cell>
-                        <Table.Cell>{reps}</Table.Cell>
-                        <Table.Cell>[{plates.join(", ")}]</Table.Cell>
-                      </Table.Row>
-                    );
-                  }
-                )}
-              </Table.Body>
-            </Table>
-          </div>
-        ) : null}
-      </Container>
-    );
-  }
-}
-
-ReactDOM.render(<SetCalculator />, document.getElementById("app"));
+const container = document.getElementById("app");
+const root = createRoot(container);
+root.render(<SetCalculator />);
